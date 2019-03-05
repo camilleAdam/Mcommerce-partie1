1c7a3f7e6cff15f2517936f3c2c4820c7bc13f46
camilleadam006 <cam.adam@outlook.fr>
2019-03-05 15:53:00 +0100

    remove unused method

--- src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
+++ src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
@@ -15,7 +15,7 @@ public interface ProductDao extends JpaRepository<Product, Integer> {
 
     List<Product> findByPrixGreaterThan(int prixLimit);
 
-    List<Product> findByNomLike(String recherche);
+//    List<Product> findByNomLike(String recherche);
 
     @Query("SELECT id, nom, prix FROM Product p WHERE p.prix > :prixLimit")
     List<Product>  chercherUnProduitCher(@Param("prixLimit") int prix);
